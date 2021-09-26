f6def0c6c5856b91019fee12202ea636a627d5a4
Petr Stukalov <petr.stukalov@intel.com>
2021-09-26 13:58:55 +0300

    add http tests

--- locustfile.py
+++ locustfile.py
@@ -1,21 +1,31 @@
 # https://www.opennet.ru/base/net/iptables_treasures.txt.html
-#locust -f locustfile.py
-from locust import User, task
+# locust -f locustfile.py
+from locust import User, task, between
+from locust.clients import HttpSession
+
 from k8s_client import K8sClient
 import time
 
 
 class K8sUser(User):
+    wait_time = between(1, 30)
+
     def __init__(self, environment):
         super().__init__(environment)
         self.client = K8sClient()
+        print(self.host)
+        self.session = HttpSession(
+            base_url="http://%s:%s/" % (self.host, self.client.id),
+            request_event=self.environment.events.request,
+            user=self
+        )
 
-    @task
-    def do(self):
+    def deploy(self):
+        is_ok = True
         start_time = time.perf_counter()
         request_meta = {
-            "request_type": "grpc",
-            "name": "Test111",
+            "request_type": "K8S",
+            "name": "Deploy",
             "response_length": 0,
             "exception": None,
             "context": None,
@@ -25,19 +35,51 @@ class K8sUser(User):
         try:
             self.client.deploy()
             self.client.service()
+            request_meta["response"] = "OK"
+            request_meta["response_length"] = 2
+        except Exception as e:
+            request_meta["exception"] = e
+            is_ok = False
+
+        request_meta["response_time"] = (time.perf_counter() - start_time) * 1000
+        self.environment.events.request.fire(**request_meta)
+        return is_ok
+
+    def delete(self):
+        is_ok = True
+        start_time = time.perf_counter()
+        request_meta = {
+            "request_type": "K8S",
+            "name": "Delete",
+            "response_length": 0,
+            "exception": None,
+            "context": None,
+            "response": None,
+        }
+
+        try:
             self.client.delete()
             request_meta["response"] = "OK"
             request_meta["response_length"] = 2
         except Exception as e:
             request_meta["exception"] = e
+            is_ok = False
 
         request_meta["response_time"] = (time.perf_counter() - start_time) * 1000
         self.environment.events.request.fire(**request_meta)
+        return is_ok
+
+    @task
+    def do(self):
+        if self.deploy():
+            for x in range(1000):
+                self.session.get("/", name="Test")
+            if self.client.deployed:
+                self.delete()
 
     def on_stop(self):
         if self.client.deployed:
             self.client.delete()
 
-
-#k = K8sClient()
-#k.deploy()
+# k = K8sClient()
+# k.deploy()
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import requests
+
 from k8s_client import K8sClient
 
-client = K8sClient()
-client.deploy()
-client.service()
-client.delete()
+
+
+# client = K8sClient()
+# client.deploy()
+# client.service()
+# client.delete()
+
