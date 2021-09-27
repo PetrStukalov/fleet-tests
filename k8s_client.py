62ae531244296ca93f5675199b2cc2ce30d4f70b
Petr Stukalov <petr.stukalov@intel.com>
2021-09-27 14:05:14 +0300

    kubectl

--- k8s_client.py
+++ k8s_client.py
@@ -14,7 +14,7 @@ class K8sClient:
     def __init__(self):
         K8sClient.id += 1
         self.id = K8sClient.id
-        self.deployed=False
+        self.deployed = False
         # ssh -v -N -L 6443:10.0.0.85:6443 ec2-user@18.195.72.99
 
     def __read_deploy(self):
@@ -36,12 +36,25 @@ class K8sClient:
         print(output)
         print(err)
 
+    def wait(self):
+        my_env = os.environ.copy()
+        my_env["NO_PROXY"] = "*"
+        cmd = "kubectl wait --for=condition=available --timeout=60s deployment/nginx-deployment%s -n default" % self.id
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE, encoding='utf8', env=my_env, shell=True)
+        if p.returncode != 0:
+            output = p.stdout.read()
+            err = p.stderr.read()
+            print(output)
+            print(err)
+            raise Exception(err, output)
+
     def deploy(self):
-        deploy=self.__read_deploy()
+        deploy = self.__read_deploy()
         self.__exec(deploy, "apply")
         self.deployed = True
+        self.wait()
 
     def delete(self):
-        deploy=self.__read_deploy()
+        deploy = self.__read_deploy()
         self.__exec(deploy, "delete")
         self.deployed = False
